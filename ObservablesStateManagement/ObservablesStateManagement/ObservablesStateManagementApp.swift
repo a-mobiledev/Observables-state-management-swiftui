1c00be40e2951931cf27474493bd9fd4c80daca2
Asad <asad.mobiledev@gmail.com>
2024-12-25 18:04:53 +0500

    Project setup

---
+++ ObservablesStateManagement/ObservablesStateManagement/ObservablesStateManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservablesStateManagementApp.swift
+//  ObservablesStateManagement
+//
+//  Created by Asad Mehmood on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ObservablesStateManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
